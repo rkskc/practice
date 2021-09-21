65a9da5a86557ed4d41540fe5b694681a36f9830
Rhiannon Savage <rhiannon.savage@kinandcarta.com>
2021-09-21 14:46:46 -0500

    demonstrate that async doesn't play nicely with functional components

--- learn-react-seasons/src/index.js
+++ learn-react-seasons/src/index.js
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const App = () => {
+	window.navigator.geolocation.getCurrentPosition(
+		position => {
+			console.log(position)
+		}, error => console.log(error)
+	)
+
 	return <div>Hello, World!</div>
 }
 
