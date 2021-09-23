86722424bc8b58f16682c483e20c61e4df6f6d6c
Rhiannon Savage <rhiannon.savage@kinandcarta.com>
2021-09-23 13:11:32 -0500

    refactor App to class-based and use state, following example

--- learn-react-seasons/src/index.js
+++ learn-react-seasons/src/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		position => {
-			console.log(position)
-		}, error => console.log(error)
-	)
+class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = { 
+			latitude: null,
+			errorMessage: ''
+		}
+	}
 
-	return <div>Hello, World!</div>
+	componentDidMount() {
+		window.navigator.geolocation.getCurrentPosition(
+			position => this.setState({ latitude: position.coords.latitude }),
+			error => this.setState({ errorMessage: error.message })
+		)
+	}
+
+	render() {
+		if(this.state.latitude) {
+		 	return <div>Latitude: {this.state.latitude}</div>
+		}
+		if(this.state.errorMessage) {
+			return <div>Error: {this.state.errorMessage}</div>
+		}
+		return <div>Loading...</div>
+	}
 }
 
 ReactDOM.render(
